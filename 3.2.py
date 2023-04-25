8c30bdca1dca3db5a2b5e030b1e2a512b3b262b5
Matvei Grishel <mvgrishel@gmail.com>
2023-04-25 20:36:11 +0300

    Пользователь вводит 3 числа, найти среднее арифметическое с точностью 3

---
+++ 3.2.py
@@ -0,0 +1,6 @@
+a = input('Enter 1-st number: ')
+b = input('Enter 2-nd number: ')
+c = input('Enter 3-rd number: ')
+result = (int(a) + int(b) + int(c))/3
+result = result.__round__(3)
+print(result)
