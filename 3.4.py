3eebfef81e349a7a1f3fd2db428bc7618706512f
Matvei Grishel <mvgrishel@gmail.com>
2023-04-25 20:43:53 +0300

    Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных

---
+++ 3.4.py
@@ -0,0 +1,5 @@
+text = input('Введите три числа через пробел: ')
+negative = text.count('-',)
+print('Отрицательных чисел: ', negative)
+positive = 3 - negative
+print('Положительных чисел: ', positive)
